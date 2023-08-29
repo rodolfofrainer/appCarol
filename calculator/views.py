efd5cf568f383d52ff4bb3aba4eae5b2df6a8e4d
Rodolfo F <rodolfo_frainer@hotmail.com>
2023-08-29 12:05:39 +0200

    remove unecessary contex in base view

--- calculator/views.py
+++ calculator/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def base(request):
-    context = {
-        'user': request.user, 
-        'is_user_authenticated': request.user.is_authenticated
-        }
-    return render(request, 'home.html', context=context)
+    return render(request, 'home.html')
