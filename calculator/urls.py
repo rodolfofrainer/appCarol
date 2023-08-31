5de4b3dfa62a18f719b66c6737192835bd317021
Rodolfo F <rodolfo_frainer@hotmail.com>
2023-08-31 11:41:53 +0200

    update html routes

--- calculator/urls.py
+++ calculator/urls.py
@@ -4,5 +4,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.baseView, name='home')
+    path('', views.baseView, name='home'),
+    path('market', views.marketPageView, name='market_page'),
+    path('createItem' , views.createItemPageView, name='create_item'),
+    path('mywage', views.myWagePageView, name='mywage_page'),
+    path('comparison', views.comparisonPageView, name='comparison_page'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def baseView(request):
     return render(request, 'home.html')
+
+def marketPageView(request):
+    return render(request, 'market.html')
+
+def comparisonPageView(request):
+    return render(request, 'comparison.html')
+
+def createItemPageView(request):
+    return render(request, 'itemCreation.html')
+
+def myWagePageView(request):
+    return render(request, 'wageHour.html')
