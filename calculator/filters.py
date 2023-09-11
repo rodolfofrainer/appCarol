6581a9df83bbbb5485e478fb30cf515591b489f0
Rodolfo F <rodolfo_frainer@hotmail.com>
2023-09-11 20:25:08 +0200

    restore db

--- calculator/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import ItemCreatedModel
-
-class ItemCreatedFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='contains')
-
-    class Meta:
-        model = ItemCreatedModel
-        fields = ['price', 'market_id']
