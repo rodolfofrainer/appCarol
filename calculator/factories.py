d78fbb872f09b7dffce3b09812a8e59c47c8805a
Rodolfo F <rodolfo_frainer@hotmail.com>
2023-09-14 18:01:54 +0200

    fix bug preventing tests run from factories

--- calculator/factories.py
+++ calculator/factories.py
@@ -1,40 +1,43 @@
-# import random
-# import string
-# import factory
-# from faker import Faker
-# from .models import UserProfileModel, MarketCreatedModel, ItemCreatedModel
-# from django.contrib.auth.models import User
+import random
+import string
+import factory
+from faker import Faker
+from .models import UserProfileModel, MarketCreatedModel, ItemCreatedModel
+from django.contrib.auth.models import User
 
-# fake = Faker()
+fake = Faker()
 
-# class UserFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = User
-    
-#     username = factory.Faker('user_name')
-#     email = factory.Faker('email')
-#     password = factory.Faker('password')
 
-# class UserProfileFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = UserProfileModel
-    
-#     user = factory.SubFactory(UserFactory)
-#     wage = factory.Faker('pydecimal', left_digits=5, right_digits=2)
-    
-# class MarketCreatedFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = MarketCreatedModel
-    
-#     name=factory.Faker('company')
-#     distance=factory.Faker('pyint', min_value=1, max_value=100)
-#     favorite=factory.Faker('boolean')
-#     user_id=factory.SubFactory(UserFactory)
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
 
-# class ItemCreatedFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = ItemCreatedModel
-    
-#     name = factory.Faker('word')
-#     price = factory.Faker('pydecimal', left_digits=3, right_digits=2)
-#     market_id = factory.SubFactory(MarketCreatedFactory)
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+    password = factory.Faker('password')
+
+
+class UserProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = UserProfileModel
+    user = factory.SubFactory(UserFactory)
+    wage = factory.Faker('pydecimal', left_digits=5, right_digits=2)
+
+
+class MarketCreatedFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = MarketCreatedModel
+
+    name = factory.Faker('company')
+    distance = factory.Faker('pyint', min_value=1, max_value=100)
+    favorite = factory.Faker('boolean')
+    user_id = factory.SubFactory(UserFactory)
+
+
+class ItemCreatedFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ItemCreatedModel
+
+    name = factory.Faker('word')
+    price = factory.Faker('pydecimal', left_digits=3, right_digits=2)
+    market_id = factory.SubFactory(MarketCreatedFactory)
--- calculator/tests.py
+++ calculator/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import MarketCreatedModel, ItemCreatedModel
+from .factories import UserFactory, MarketCreatedFactory, ItemCreatedFactory
 from faker import Faker
 import random
 
@@ -9,22 +10,11 @@ fake = Faker()
 
 class ORM_Create_update_delete_test(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(
-            username=fake.user_name(),
-            password=fake.password()
-        )
-        self.market = MarketCreatedModel.objects.create(
-            name=fake.name(),
-            distance=random.randint(1, 80),
-            favorite=False,
-            user_id=self.user)
-        self.market2 = MarketCreatedModel.objects.create(
-            name=fake.name(),
-            distance=random.randint(1, 80),
-            favorite=False,
-            user_id=self.user)
-        self.item = ItemCreatedModel.objects.create(
-            name=fake.name(), price=random.uniform(0.01, 5000.0), market_id=self.market)
+        self.user = UserFactory()
+        
+        self.market = MarketCreatedFactory()
+        self.market2 = MarketCreatedFactory()
+        self.item = ItemCreatedFactory()
 
     def test_creation(self):
         # if data was created
@@ -42,6 +32,7 @@ class ORM_Create_update_delete_test(TestCase):
         # if market 2 is favorited, market 1 is unfavorited
         self.market2.favorite = True
         self.market.save()
+        self.market.refresh_from_db()
         self.assertEqual(self.market.favorite, False)
 
     def tearDown(self):
